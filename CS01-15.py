03db012f9b9e11a78050a8f43bbc0c42268f11f9
Salor69 <87053983+Salor69@users.noreply.github.com>
2021-08-18 12:08:06 +0700

    def my_function

---
+++ CS01-15.py
@@ -0,0 +1,9 @@
+def my_function(x):
+    for i in range(len(x)):
+        print(i)
+        if (x[i] == 20):
+            x[i] = 200
+    print(x)
+thislist = [5,10,15,20,25,50,20]
+my_function(thislist)
+
